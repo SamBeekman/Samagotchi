6043c57061e2b4f7035f724cfa384d083fe742cc
SamBeekman <sambeekman@hotmail.com>
2023-10-20 15:22:06 +1100

    frontend skeleton

---
+++ Samagotchi/client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// import { gql } from '@apollo/client';
+
+// // may need to be QUERY_ME
+// export const GET_ME = gql`
+//     query me {
+//         me {
+//             _id
+//             username
+//             email
+//             bookCount
+//             savedBooks {
+//                 bookId
+//                 title
+//                 authors
+//                 description
+//                 image
+//                 link
+//             }
+//         }
+//     }
+// `;
