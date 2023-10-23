2af2b7e141b7499268c5bd72143fe149abf00ff2
SamBeekman <sambeekman@hotmail.com>
2023-10-23 20:13:38 +1100

    setup main and app on client

--- Samagotchi/client/src/main.jsx
+++ Samagotchi/client/src/main.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-import './index.css'
+import Chat from './pages/Chat'
+import Friends from './pages/Friends'
+import Gameplay from './pages/Gameplay'
+import Homepage from './pages/Homepage'
+import LoginSignup from './pages/LoginSignup'
+import Payment from './pages/Payment'
+import Profile from './pages/Profile'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 >Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      },
+      {
+        path: '/chat',
+        element: <Chat />
+      },
+      {
+        path: '/friends',
+        element: <Friends />
+      },
+      {
+        path: '/gameplay',
+        element: <Gameplay />
+      },
+      {
+        path: '/login',
+        element: <LoginSignup />
+      },
+      {
+        path: '/payment',
+        element: <Payment />
+      },
+      {
+        path: '/profile/:id',
+        element: <Profile />
+      },
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
